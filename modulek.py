3f72073d7850ab2b795788c2be8b272499196680
Viacheslav Hudzovskyi <hvv19890828@gmail.com>
2020-07-17 23:08:46 +0000

    suited up

--- modulek.py
+++ modulek.py
@@ -1,10 +1,10 @@
-class Car:
-  def __init__(self,shlyap):
-        print(shlyap)
-  jm = 33
-  js = {"wewe": "ds3"}
-  xerf = " ww"
-  def bla(self, j):
-     print(j + self.jm, self.js, self.xerf)
-     self.xerf =  "qq"
-     print(j+j, self.js, self.xerf)
+class parent:
+  def __init__(self,arg1):
+        print("printed arg1 from parent init: ", arg1)
+  int = 33
+  dict1 = {"dict_key1": "dict_value1"}
+  strng = "par_string value"
+  def func1(self, arg1):
+     print("'printed arg1 + self.int, self.dict1, self.strng'from func1 ", arg1 + self.int, self.dict1, self.strng)
+     self.strng =  "par_string value after reassigned incidev parent_func1"
+     print("printed 'arg1+arg1," ", self.dict1," ", self.strng' from func1: ", arg1+arg1," ", self.dict1," ", self.strng)
--- test.py
+++ test.py
@@ -8,24 +8,24 @@ import time
 import re
 import random as rmdm
 from math import cos as c
-import modulek as md
+import modulek as mod
 
-print(c(7))
+print("printed cos from 7 as 'c'",c(7))
 
-class srkf(md.Car):
-  def __init__(self, w, shlyapx):
-    super().__init__(shlyapx) 
-    print("initnax ",w)
-  def blab(self, fjh):
-     print(fjh)
-     self.bla(434)
+class child(mod.parent):
+  def __init__(self, arg1, arg2):
+    super().__init__(arg2) 
+    print("printed arg1 from child init ",arg1)
+  def child_func1(self, arg1):
+     print("printed arg1 from child child_func1", arg1)
+     self.func1(434)
 #    pass
 
-rrrr = md.Car("Carrrrrrrrrrrrrrrinit1")
-rrrr.bla(333333333333333)
+par_obj = mod.parent("par_obj mod.parent initiation with arg")
+par_obj.func1(333333333333333)
 
-rrr = srkf("877","Carrrrrrrrrrrrrrrinit2")
-rrr.blab("4yty334")
+child_obj = child("877","child_obj child initiation with second arg")
+child_obj.child_func1("arg1 of child_obj.func1")
 
 
 
@@ -48,35 +48,35 @@ jsonOtptFl.close()
 
 #------------------------------------
 
-print("jdhejdhejhd")
-print(3-9)
-print((9 // 3))
-print(9 % 4)
-print(9 ** 6)
+print("just a print")
+print("3-9: ",3-9)
+print((" 9 // 3: ", 9 // 3))
+print("remainder 9 % 4: ",9 % 4)
+print("rise 9 to the 6 power: ",9 ** 6)
 
 
-hg = " dsd"
-js = 23
-print("fdfjdjfhjd ",str (float(js)),hg)
+just_an_str = "just_an_str"
+just_an_int = 23
+print("print str (float(js)),just_an_str): ",str (float(just_an_int)),just_an_str)
 
-nud = input("enter firstjhh:")
+inpt = str(500) # input("enter firstjhh:")
 
-print(nud + hg)
+print("print inpt + just_an_str: ",inpt + just_an_str)
 
 
-if nud == str(0):
-   print("DADADA")
-elif nud != str(1):
-  print("pasiryodke")
+if inpt == str(0):
+   print("if inpt == str(0):")
+elif inpt != str(1):
+  print("elif inpt != str(1):")
 else:
- print("niachom")
+ print("else:")
 
-for i in nud:
-   print(i, "\n")
+for i in inpt:
+   print("Print from for loop: ",i, "\n")
 
 j=0
 while j < 10:
-   print(j)
+   print("Print from while loop: ",j)
 #   j-=1
    j+=1
 
@@ -84,17 +84,16 @@ while j < 10:
 arry = [32323,545445,232535,["no,d",3,3434.4344,[{"wew": "uyuyuuyu"}]]]
 cort = ("dasdsd",'sdsds',2323)
 dict = ({"dict1": "dict value"})
-print(dict['dict1'])
-
+print("print("", dict['dict1'])" + dict['dict1'])
 plur = {"434","erer"}
 
 def xoxo(stri):
   return stri
-xoxo(nud)
+xoxo(inpt)
 
 try:
  lya =  lambda x, y: print(x - y)
- lya(int(nud), float(nud))
+ lya(int(inpt), float(inpt))
 except:
  print("except: r u fcking out of your fcking mind, and don't you see it acceps integer value only!!!?")
 else:
