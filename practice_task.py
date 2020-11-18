4cfbba705304894ff994721b7763d228b0b3029e
hvv19890828 <36333626+hvv19890828@users.noreply.github.com>
2020-11-18 18:15:39 +0200

    Update practice_task.py

--- practice_task.py
+++ practice_task.py
@@ -10,7 +10,7 @@ tdisk = shutil.disk_usage("/")[2] // (2**30)
 tfiles = len(next(os.walk("/etc"))[2])
 payload = {'current_date': current_date, 'api_version': 'v1', 'tdisk': tdisk, 'tfiles': tfiles}
 
-r = requests.post('http://foo.bar/test.php', data=payload)
+r = requests.post('http://test.bar/test.php', data=payload)
  
 with open("/tmp/response.json", mode='w', encoding='utf_8') as responceLog:
  json.dump(r.json(), responceLog)
