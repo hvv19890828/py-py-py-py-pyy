3592c05568eb7e9df76b915fb47f37cc85adfc98
Viacheslav Hudzovskyi <hvv19890828@gmail.com>
2022-08-19 20:03:12 +0300

    Code grooming

--- docker_image_cleaner.py
+++ docker_image_cleaner.py
@@ -14,46 +14,45 @@ import re
 from datetime import datetime
 from dateutil.parser import isoparse
 
+
+def match_repo_name(tag_tmplt, tag_list):
+   repo_name = str(tag_list)
+   repo_name_match = re.findall(tag_tmplt, repo_name)
+   return repo_name_match
+
+def time_diff(image_list, t_fmt):
+   creation_date = str(isoparse(image_list.attrs["Created"]))
+   creation_date_fmtd = re.sub(r"\..*", "", creation_date)
+   current_date = str(datetime.now())
+   current_date_fmtd = re.sub(r"\..*", "", current_date)
+   tstamp1 = datetime.strptime(current_date_fmtd, t_fmt)
+   tstamp2 = datetime.strptime(creation_date_fmtd, t_fmt)
+   tdiff = tstamp1 - tstamp2
+   tdiff_mins = int(round(tdiff.total_seconds() / 60))
+   return tdiff_mins
+
+
 # Termination filters prompt
 tag_pattern = input('Enter image tag pattern: ')
 termination_age = int(input('Enter termination age (in minutes): '))
 imgs_to_save = int(input('Enter a number of images to preserve: '))
-
-client = docker.from_env()
-images_i = client.images.list()
-
 time_fmt = '%Y-%m-%d %H:%M:%S'
+client = docker.from_env()
+
 for i in client.images.list():
    # Image match markers collection
-   repo_name_i = str(i.tags)
-   repo_name_match_i = re.findall(tag_pattern, repo_name_i)
+   repo_name_match_i = match_repo_name(tag_pattern, i.tags)
    # Image age calculation
-   creation_date_i = str(isoparse(i.attrs["Created"]))
-   creation_date_fmtd_i = re.sub(r"\..*", "", creation_date_i)
-   current_date_i = str(datetime.now())
-   current_date_fmtd_i = re.sub(r"\..*", "", current_date_i)
-   tstamp1_i = datetime.strptime(current_date_fmtd_i, time_fmt)
-   tstamp2_i = datetime.strptime(creation_date_fmtd_i, time_fmt)
-   tdiff_i = tstamp1_i - tstamp2_i
-   tdiff_mins_i = int(round(tdiff_i.total_seconds() / 60))
+   tdiff_mins_i = time_diff(i, time_fmt)
 
    # Image deletion approval
    deletion_approval = False
    img_amount = 0
-   images_j = client.images.list()
    for j in client.images.list():
       # Image match markers collection
-      repo_name_j = str(j.tags)
-      repo_name_match_j = re.findall(tag_pattern, repo_name_j)
+      repo_name_match_j = match_repo_name(tag_pattern, j.tags)
       # Image age calculation
-      creation_date_j = str(isoparse(j.attrs["Created"]))
-      creation_date_fmtd_j = re.sub(r"\..*", "", creation_date_j)
-      current_date_j = str(datetime.now())
-      current_date_fmtd_j = re.sub(r"\..*", "", current_date_j)
-      tstamp1_j = datetime.strptime(current_date_fmtd_j, time_fmt)
-      tstamp2_j = datetime.strptime(creation_date_fmtd_j, time_fmt)
-      tdiff_j = tstamp1_j - tstamp2_j
-      tdiff_mins_j = int(round(tdiff_j.total_seconds() / 60))
+      tdiff_mins_j = time_diff(j, time_fmt)
       # Comparison
       if len(repo_name_match_j) > 0 and tdiff_mins_i >= tdiff_mins_j:
          img_amount += 1
@@ -66,5 +65,5 @@ for i in client.images.list():
    image_id = (str(i.id).replace("sha256:", ""))
    if tdiff_mins_i >= termination_age and deletion_approval and len(repo_name_match_i) > 0 :
       client.images.remove(image=image_id, force=True)
-      print("Deleted: " + repo_name_i + " " + str(i.id))
+      print("Deleted: " + str(i.tags)  + " " + str(i.id))
 
