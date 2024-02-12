5adbb3f0fda6e9a1e74b9d6f82ae4d63e6b104cf
Viacheslav Hudzovskyi <hvv19890828@gmail.com>
2024-02-13 00:45:03 +0200

    minor

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from math import cos as c
       
 # in case of issues with pip: curl https://bootstrap.pypa.io/get-pip.py | python3
 import mysql.connector  # pip3 install mysql-connector-python
-import requests  # pip3 install requests may be required
+import requests  # pip3 install requests may be required.
       
 import modulek as mod
 import time
